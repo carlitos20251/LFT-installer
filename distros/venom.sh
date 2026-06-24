dfd39180b4ea33e795793b2c6094631bf3748792
carpana <94392493+carlitos20251@users.noreply.github.com>
2026-06-24 11:22:56 -0500

    Refactor partitioning logic for disk formatting

--- distros/venom.sh
+++ distros/venom.sh
@@ -5,13 +5,7 @@ DISK="$1"
 
 # --- Formatear el disco ---
 echo -e "\n[+] Formateando el disco $DISK..."
-if [[ "$DISK" =~ nvme|mmcblk ]]; then
-    EFI="${DISK}p1"
-    ROOT="${DISK}p2"
-else
-    EFI="${DISK}1"
-    ROOT="${DISK}2"
-fi
+
 umount "${DISK}"* 2>/dev/null || true
 
 # Crear tabla de particiones (GPT)
@@ -20,7 +14,13 @@ parted -s "$DISK" mklabel gpt
 # Crear particiones (EFI + Raíz)
 parted -s "$DISK" mkpart primary fat32 1MiB 512MiB  # EFI
 parted -s "$DISK" mkpart primary ext4 512MiB 100%   # Raíz
-
+if [[ "$DISK" =~ nvme|mmcblk ]]; then
+    EFI="${DISK}p1"
+    ROOT="${DISK}p2"
+else
+    EFI="${DISK}1"
+    ROOT="${DISK}2"
+fi
 # Formatear particiones
 mkfs.fat -F32 "$EFI"
 mkfs.ext4 "$ROOT"
