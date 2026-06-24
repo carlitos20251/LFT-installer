986930367c8ecc64db412695cdaaa21fd8a15f01
carpana <94392493+carlitos20251@users.noreply.github.com>
2026-06-24 09:05:20 -0500

    Fix partition formatting and mounting commands

--- distros/debian.sh
+++ distros/debian.sh
@@ -15,14 +15,14 @@ parted -s "$DISK" mkpart primary fat32 1MiB 512MiB  # EFI
 parted -s "$DISK" mkpart primary ext4 512MiB 100%   # Raíz
 
 # Formatear particiones
-mkfs.fat -F32 "${DISK}p1" || mkfs.fat -F32 "${DISK}1"
-mkfs.ext4 "${DISK}p2" || mkfs.ext4 "${DISK}2"
+mkfs.fat -F32 "${DISK}1"
+mkfs.ext4 "${DISK}2"
 
 # Montar particiones
 mkdir -p /mnt
-mount "${DISK}p2" /mnt || mount "${DISK}2" /mnt
+mount "${DISK}2" /mnt
 mkdir -p /mnt/boot
-mount "${DISK}p1" /mnt/boot || mount "${DISK}1" /mnt/boot
+mount "${DISK}1" /mnt/boot
 
 # --- Instalación de Debian ---
 echo -e "\n[+] Instalando Debian..."
